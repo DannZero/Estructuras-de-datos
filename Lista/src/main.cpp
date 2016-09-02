45bfa4ccf9a55decacd03fa180b8c6f976a1149d
Filcer <gabfiltzer@gmail.com>
2016-09-02 01:48:45 -0500

    Update main.cpp

--- Lista/src/main.cpp
+++ Lista/src/main.cpp
@@ -1,14 +1,13 @@
-#include "Lista.h"
+#include "Lista.h" 	//se incluye la clase lista
 using namespace std;
 
-int main()
+int main() 	//declaracion de clase main
 {
-	Lista l = Lista();
-	l.addInicio(4);
+	Lista l = Lista(); //se crea la lista
+	l.addInicio(4); //se añaden los primeros cuatro nodos
 	l.addInicio(5);
 	l.addInicio(6);
 	l.sddInicio(7);
-	l.show();
-	cout<<"Hello World"<<endl;
-	return 0;
+	l.show();	//se imprime la lista
+	return 0; 	//termina el programa
 }

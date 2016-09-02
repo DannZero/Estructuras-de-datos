28cd6f2fc1245a8e189f69fdc613d20833de677d
Filcer <gabfiltzer@gmail.com>
2016-09-02 01:14:20 -0500

    Update Nodo.h

--- Lista/include/Nodo.h
+++ Lista/include/Nodo.h
@@ -1,16 +1,16 @@
 #include <iostream>
 
-class Nodo
+class Nodo //se crea la clase "nodo"
 {
 private:
-	int Dato;
-	Nodo* Sig;
+	int Dato; //se declara la variable donde se ingresara la información de los nodos
+	Nodo* Sig; //se declara el apuntador que apuntara entre los nodos.
 public:
-	Nodo(int Dato, Nodo* Sig);
-	Nodo(int Dato);
-	void setDato(int Dato);
-	void setSig(Nodo* Sig);
-	int getDato();
-	Nodo* getSig();
+	Nodo(int Dato, Nodo* Sig); //metodo constructor del primer nodo (pila) y los siguientes
+	Nodo(int Dato); //metodo constructor del ultimo nodo (cola)
+	void setDato(int Dato); //metodo para asignar el dato a un nodo
+	void setSig(Nodo* Sig); //metodo para apuntar al siguiente nodo de la lista
+	int getDato(); //metodo para leer el dato
+	Nodo* getSig(); //metodo para leer el siguiente nodo de la lista que se apunta
 	//~Nodo();
 };

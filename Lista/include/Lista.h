7e4fd717c12dc6efdd77924a0f0a68b09f9e6bbd
Filcer <gabfiltzer@gmail.com>
2016-09-02 01:10:20 -0500

    Update Lista.h

--- Lista/include/Lista.h
+++ Lista/include/Lista.h
@@ -3,18 +3,18 @@
 class Lista
 {
 public:
-	Nodo* H;
-	Nodo* T;	
-	Lista();
-	Lista(int Dato);
-	bool isEmpty();
-	void addInicio(int Dato);
-	void addFinal(int Dato);
-	//void addRef(int Dato, int Ref);
-	int removeInicio();
-	int removeFinal();
-	//removeRef();
-	//int vaciarLista();
-	Nodo* hasElement();
-	void show();
+	Nodo* H; //se declara el apuntador a pila
+	Nodo* T; //se declara el apuntador a cola
+	Lista(); //se crea una lista
+	Lista(int Dato); //se asigna un dato a la lista nueva
+	bool isEmpty(); //metodo para comprobar si la lista está vacía
+	void addInicio(int Dato); //metodo para agregar un nodo al inicio
+	void addFinal(int Dato); //metodo para agregar un nodo al final
+	//void addRef(int Dato, int Ref); //metodo para agregar un nodo en una posicion especifica
+	int removeInicio(); //metodo para eliminar el primer nodo (pila)
+	int removeFinal(); //metodo para eliminar el ultimo nodo (cola)
+	//removeRef();    //metodo para eliminar un nodo especifico
+	int vaciarLista(); //metodo para borrar los datos de toda la lista
+	Nodo* hasElement(); //metodo para buscar un elemento especifico
+	void show(); //metodo para imprimir un nodo
 };

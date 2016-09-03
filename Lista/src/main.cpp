8b3afc188797f07d4f631621b1c72d82081cd167
Daniel Ponce de León <dannzero@gmail.com>
2016-09-02 20:15:13 -0500

    Main completo
    Se usan todos los métodos

--- Lista/src/main.cpp
+++ Lista/src/main.cpp
@@ -1,12 +1,22 @@
 #include "Lista.h" //se incluye la clase lista
 
-int main() //declaracion de clase main
+int main()
 {
     Lista l = Lista(); //se crea la lista
-    l.addInicio(4);    //se añaden los primeros cuatro nodos
-    l.addInicio(5);
-    l.addInicio(6);
-    l.addInicio(7);
+    l.addInicio(4);
+    l.addFinal(5);
+    l.addFinal(7);
+    std::cout<<l.hasElement(5)->getDato()<<std::endl;
     l.show(); //se imprime la lista
+    l.addRef(6,3);
+    l.show();
+    l.removeInicio();
+    l.show();
+    l.removeFinal();
+    l.show();
+    l.removeRef(2);
+    l.show();
+    l.vaciarLista();
+    
     return 0; //termina el programa
 }
